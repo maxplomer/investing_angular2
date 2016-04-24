9a9c752f8ef94d2bad730c5aca62ae991d73c1bd
Max Plomer <maxplomer@gmail.com>
2016-04-24 17:47:56 -0400

    make API call to post to /users

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {Http, Response} from 'angular2/http';
 import {Observable} from 'rxjs/Rx';
 import {MyGlobalService} from './myglobal.service';
+import {ContentHeaders} from './headers';
 
 @Component({
     selector: 'my-app',
@@ -56,7 +57,7 @@ export class AppComponent {
         this.login()
         break;
       case 'register':
-        this.register()
+        this.register(this.newUser.email, this.newUser.password);
         break;
       default:
         console.log("submitAuthForm called without formAction")
@@ -70,8 +71,18 @@ export class AppComponent {
     console.log("calling login()");
   }
 
-  register() {
-    console.log("calling register()");
+  register(email, password) {
+    let body = JSON.stringify({ email, password });
+    this.http.post(this.apiDomain + '/users', body, { headers: ContentHeaders })
+      .subscribe(
+        response => {
+          //localStorage.setItem('jwt', response.json().id_token);
+          // Fade out Login menu and show user stats
+        },
+        error => {
+          console.log(error.text());
+        }
+      );
   }
 
 }
