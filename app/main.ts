f55193e89021867be53423ad5e175f0b57e91b75
Max Plomer <maxplomer@gmail.com>
2016-04-25 01:26:04 -0400

    Revert "add AuthHttp"
    This reverts commit fdced7c1a142d2968dbd4eab21962f05fd148c9a.

--- app/main.ts
+++ app/main.ts
@@ -1,18 +1,8 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {provide} from 'angular2/core';
-import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import 'rxjs/add/operator/map';
 import {AppComponent} from './app.component';
-import {AuthConfig, AuthHttp} from 'angular2-jwt';
 
 bootstrap(AppComponent, [
-  HTTP_PROVIDERS,
-  provide(AuthHttp, {
-    useFactory: (http) => {
-      return new AuthHttp(new AuthConfig({
-        tokenName: 'jwt'
-      }), http);
-    },
-    deps: [Http]
-  })
+  HTTP_PROVIDERS
 ]);
