7437003e2dc40a2cd1e0e79196496e02c59269d3
Max Plomer <maxplomer@gmail.com>
2016-04-27 18:44:18 -0400

    manually add .js extension, AuthHttp uses Angular 2's Http class but includes the Authorization header for you on requests

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,17 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {Component, View, provide} from 'angular2/core';
-import {RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
-import {HTTP_PROVIDERS, Http} from 'angular2/http';
-import {AuthHttp, tokenNotExpired} from 'angular2-jwt';
 import 'rxjs/add/operator/map';
 import {AppComponent} from './app.component';
+import {Component, View, provide} from 'angular2/core';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {AuthHttp, AuthConfig, tokenNotExpired, JwtHelper} from 'angular2-jwt';
+
 
 bootstrap(AppComponent, [
-  HTTP_PROVIDERS
+  HTTP_PROVIDERS,
+  provide(AuthHttp, { 
+    useFactory: (http) => {
+      return new AuthHttp(new AuthConfig(), http);
+    },
+    deps: [Http]
+  })
 ]);
