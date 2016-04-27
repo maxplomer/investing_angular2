d1653d0a102d60a0e740b1c48d12c7dcc9afd3ab
Max Plomer <maxplomer@gmail.com>
2016-04-27 01:42:41 -0400

    Import dependencies

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,8 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import {Component, View, provide} from 'angular2/core';
+import {RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {AuthHttp, tokenNotExpired} from 'angular2-jwt';
 import 'rxjs/add/operator/map';
 import {AppComponent} from './app.component';
 
