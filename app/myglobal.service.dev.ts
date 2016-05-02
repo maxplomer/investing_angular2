dd1e35b7eabd2bb25cec12873193692ea591f49b
Max Plomer <maxplomer@gmail.com>
2016-05-02 17:41:54 -0400

    use single quotes

--- app/myglobal.service.dev.ts
+++ app/myglobal.service.dev.ts
@@ -2,7 +2,7 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class MyGlobalService {
-  getApiDomain = () => "http://localhost:3001";
+  getApiDomain = () => 'http://localhost:3001';
   getAuth0ClientId = () => '66lkhr6nngfcbIpsgXRbP0fSyDWFtzbM';
   getAuth0Domain = () => 'maxplomer.auth0.com';
 }
