184e9ca9e84da5ac5c0546f853b2e20e6402156f
Max Plomer <maxplomer@gmail.com>
2016-05-02 17:16:12 -0400

    move auth0 client id and domain to global service

--- app/myglobal.service.dev.ts
+++ app/myglobal.service.dev.ts
@@ -3,5 +3,7 @@ import {Injectable} from 'angular2/core';
 @Injectable()
 export class MyGlobalService {
   getApiDomain = () => "http://localhost:3001";
+  getAuth0ClientId = () => '66lkhr6nngfcbIpsgXRbP0fSyDWFtzbM';
+  getAuth0Domain = () => 'maxplomer.auth0.com';
 }
 
