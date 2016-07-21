eaebbd75480ef8cff6c6ce4165b90c75d25b6408
Max Plomer <maxplomer@gmail.com>
2016-07-21 16:17:08 -0400

    restart backend server, new ip

--- app/myglobal.service.ts
+++ app/myglobal.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class MyGlobalService {
-  getApiDomain = () => 'http://54.186.85.251:24413';
+  getApiDomain = () => 'http://54.187.163.195:24413';
   getAuth0ClientId = () => '66lkhr6nngfcbIpsgXRbP0fSyDWFtzbM';
   getAuth0Domain = () => 'maxplomer.auth0.com';
 }
