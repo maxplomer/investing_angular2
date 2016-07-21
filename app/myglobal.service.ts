13bb7e9e81d664cef5638dbe951fbf6524524ec6
Max Plomer <maxplomer@gmail.com>
2016-07-21 16:25:05 -0400

    update domain again

--- app/myglobal.service.ts
+++ app/myglobal.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class MyGlobalService {
-  getApiDomain = () => 'http://54.187.170.75:24413';
+  getApiDomain = () => 'http://54.187.163.195:24413';
   getAuth0ClientId = () => '66lkhr6nngfcbIpsgXRbP0fSyDWFtzbM';
   getAuth0Domain = () => 'maxplomer.auth0.com';
 }
